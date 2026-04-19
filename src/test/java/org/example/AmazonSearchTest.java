785ed306fda77ba038e40d3df126ddadd71b1917
aitestautomation7-web <aitestautomation7-web@users.noreply.github.com>
2026-04-19 21:20:14 +0530

    Add Playwright test for Amazon tshirts search

---
+++ src/test/java/org/example/AmazonSearchTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AmazonSearchTest {
+    private Playwright playwright;
+    private Browser browser;
+    private Page page;
+
+    @BeforeClass
+    public void setUp() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        page = browser.newPage();
+    }
+
+    @Test
+    public void searchTShirtsOnAmazon() {
+        page.navigate("https://www.amazon.com/");
+        page.locator("input#twotabsearchtextbox").fill("tshirts");
+        page.locator("input#nav-search-submit-button").click();
+        // Optionally, assert that results are shown
+        page.waitForSelector("span.a-color-state.a-text-bold");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (browser != null) browser.close();
+        if (playwright != null) playwright.close();
+    }
+}
+
