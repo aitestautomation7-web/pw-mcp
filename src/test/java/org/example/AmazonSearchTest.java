0fc6640daf1947ff868753366c4d9571118824fd
aitestautomation7-web <aitestautomation7-web@users.noreply.github.com>
2026-04-20 12:29:35 +0530

    updated locatore

--- src/test/java/org/example/AmazonSearchTest.java
+++ src/test/java/org/example/AmazonSearchTest.java
@@ -14,7 +14,7 @@ public class AmazonSearchTest {
         playwright = Playwright.create();
 
         browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
+                new BrowserType.LaunchOptions().setHeadless(true)
         );
 
         page = browser.newPage();
