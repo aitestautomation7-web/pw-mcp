1ea2a508d3454cc238959d7f05b46fcf5565900a
aitestautomation7-web <aitestautomation7-web@users.noreply.github.com>
2026-04-20 11:34:34 +0530

    updated the parameter for browser

--- src/test/java/org/example/AmazonSearchTest.java
+++ src/test/java/org/example/AmazonSearchTest.java
@@ -13,7 +13,7 @@ public class AmazonSearchTest {
     @BeforeClass
     public void setUp() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         page = browser.newPage();
     }
 
--- src/test/java/org/example/PracticeTestAutomationLoginTest.java
+++ src/test/java/org/example/PracticeTestAutomationLoginTest.java
@@ -14,7 +14,7 @@ public class PracticeTestAutomationLoginTest {
     @BeforeClass
     public void setUp() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         page = browser.newPage();
     }
 
